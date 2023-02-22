305b581ccc8913e2709fef9e2b5b8caf8fe1004e
Grazielle Café <grazielle.cafe@aluno.ufop.edu.br>
2023-02-22 17:05:12 -0300

    feat: exercicios sobre fundamentos de ts

---
+++ 02-Fundamentos/index.js
@@ -0,0 +1,23 @@
+"use strict";
+// 1 - numbers
+let x = 10;
+console.log(x);
+console.log(typeof x);
+// 2 - string
+const firstName = "Grazielle";
+console.log(firstName);
+console.log(typeof firstName);
+console.log(firstName.toUpperCase());
+// 3 - boolean 
+let a = false;
+console.log(a);
+console.log(typeof a);
+// 4 - annotation e inference
+const annotation = "Teste";
+let inf = "Teste";
+console.log("testando");
+// exercício
+const n1 = 10;
+const numberToString = n1.toString();
+const printMyNumber = `O numero printado é ${numberToString}`;
+console.log(printMyNumber);
---
+++ 02-Fundamentos/index.ts
@@ -0,0 +1,30 @@
+// 1 - numbers
+let x: number = 10; 
+console.log(x); 
+console.log(typeof x); 
+
+// 2 - string
+const firstName: string = "Grazielle";
+console.log(firstName); 
+console.log(typeof firstName); 
+console.log(firstName.toUpperCase()); 
+
+// 3 - boolean 
+let a: boolean = false; 
+console.log(a); 
+console.log(typeof a); 
+
+// 4 - annotation e inference
+const annotation: string = "Teste"; 
+let inf = "Teste"; 
+
+console.log("testando"); 
+
+
+// exercício
+const n1: number = 10; 
+const numberToString = n1.toString(); 
+const printMyNumber = `O numero printado é ${numberToString}`; 
+console.log(printMyNumber); 
+
+
